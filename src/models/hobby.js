9d55caa55e46a5b9ce574f931863fcaf0c6d9ebc
mindkeeper <fcb.nyak@gmail.com>
2023-09-08 15:33:44 +0700

    adding hobbies association

--- src/models/hobby.js
+++ src/models/hobby.js
@@ -7,8 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ User }) {
       // define association here
+      this.belongsTo(User, { foreignKey: "user_id", as: "User" });
     }
   }
   Hobby.init(
