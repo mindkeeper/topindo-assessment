f882ebccc3414dc31fc37610e03de4189c1298a1
mindkeeper <fcb.nyak@gmail.com>
2023-09-08 16:43:37 +0700

    adding endpoint user detail

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const addUserHandler = require("../controllers/user/addUser");
+const getUserDetailHandler = require("../controllers/user/getUserDetail");
 
 const route = require("express").Router();
 
 route.post("/", addUserHandler);
+route.get("/:id", getUserDetailHandler);
 
 module.exports = route;
