9b004c00fe9626acb4a02557e97b4b194d301eb7
mindkeeper <fcb.nyak@gmail.com>
2023-09-08 16:20:49 +0700

    adding verify token middleware

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,9 +1,10 @@
 const route = require("express").Router();
 
+const verifyToken = require("../middlewares/verifyToken");
 const loginRoutes = require("./auth");
 const userRoutes = require("./user");
 const prefix = "/api";
 
 route.use(`${prefix}`, loginRoutes);
-route.use(`${prefix}/user`, userRoutes);
+route.use(`${prefix}/user`, verifyToken, userRoutes);
 module.exports = route;
