185f09ec28f1cdad8bae1b9e49e93ede70fa6f0a
mindkeeper <fcb.nyak@gmail.com>
2023-09-08 15:11:43 +0700

    changing timestamp name to undersored

--- db/migrations/20230908061834-create-user.js
+++ db/migrations/20230908061834-create-user.js
@@ -25,12 +25,12 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("now()"),
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("now()"),
